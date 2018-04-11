14db196125593fbfbfe29c045b1a3ee438911d31
mvanderlip <mvanderlip@student.neumont.edu>
2018-04-11 09:19:17 -0600

    Added some functionality to LayoutBuilder.cs

--- FactoryPatternAssinment/LayoutBuilder/LayoutBuilder.cs
+++ FactoryPatternAssinment/LayoutBuilder/LayoutBuilder.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace LayoutBuilder
 {
-    public class LayoutBuilder
+    public abstract class LayoutBuilder
     {
+        Stack<Component> components = new Stack<Component>();
+
+        public void AddComponent(string type, string content, float top, float left, float width, float height)
+        {
+            Component c = MakeComponent(type);
+            c.Content = content;
+            c.Top = top;
+            c.Left = left;
+            c.Width = width;
+            c.Height = height;
+            components.Push(c);
+        }
+
+        public void RemoveComponent()
+        {
+            components.Pop();
+        }
+
+        public abstract Component MakeComponent(string type);
+
+        // Brandon Stuff Method, Name to be changed as well as possibly splitting it?
+
+        public abstract void Process();
     }
 }
